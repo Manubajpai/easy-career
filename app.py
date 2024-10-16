a8a3b2d7b1aaf36604c6f56526bdd80ac00f2522
Manubajpai <prmanubajpai.77@gmail.com>
2024-10-16 18:52:03 +0530

    added jobs from database

--- app.py
+++ app.py
@@ -1,54 +1,49 @@
 from flask import Flask , render_template , jsonify
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 
-JOBS = [
-    {
-        'id' : 1,
-        'title' : 'Data Scientist',
-        'location' : 'Bengaluru , India',
-        'salary' : 'Rs 20,00,000'
-    },
-    {
-        'id' : 2,
-        'title' : 'Data Analyist',
-        'location' : 'Bengaluru , India',
-        'salary' : 'Rs 10,00,000'
-    },
-    {
-        'id' : 3,
-        'title' : 'Frontend Engineer',
-        'location' : 'Delhi , India',
-        'salary' : 'Rs 15,00,000'
-    },
-    {
-        'id' : 4,
-        'title' : 'Backend Engineer',
-        'location' : 'Bengaluru , India',
-        'salary' : 'Rs 18,00,000'
-    },
-    {
-        'id' : 5,
-        'title' : 'Data Scientist',
-        'location' : 'New York, USA',
-        'salary' : '$ 140,000'
-    },
-    {
-        'id' : 6,
-        'title' :' Data Scientist',
-        'location' : 'San Fransico, USA',
-        'salary' : '$ 120,000'
-    }
-]
 
+# config for mysql database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:db_pass_easycareer@localhost/easycareer'
+app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = False
+
+# initialise the database
+db = SQLAlchemy(app)
+
+# defining the job model
+class Jobs(db.Model):
+    id = db.Column(db.Integer , primary_key = True)
+    title = db.Column(db.String(200) , nullable = False)
+    location = db.Column(db.String(200) , nullable = False)
+    salary = db.Column(db.Integer)
+    currency = db.Column(db.String(10))
+    responsibilities = db.Column(db.String(2000))
+    requirements = db.Column(db.String(2000))
+
+# route to render jobs on home
 @app.route("/")
 def easy():
-    return render_template("home.html" , 
-                           jobs=JOBS)
+    jobs = Jobs.query.all() # fetch all jobs from database
+    return render_template("home.html" , jobs = jobs)
 
+# api route to return jobs in json format
 @app.route("/api/jobs")
 def list_jobs():
-    return jsonify(JOBS)
+    jobs = Jobs.query.all()
+    jobs_list = [
+        {
+            'id':job.id,
+            'title':job.title,
+            'location':job.location,
+            'salary':job.salary,
+            'currency':job.currency,
+            'responsibilities':job.responsibilities,
+            'requirements':job.requirements
+        } for job in jobs
+    ]
+    return jsonify(jobs_list)
 
 if __name__ == "__main__":
     app.run(debug=True)
